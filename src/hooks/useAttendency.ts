6ee547c6e81accad51570b4a3544924103f4e378
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-08 21:47:49 +0200

    feat: add useAttendecy hook

--- src/hooks/useAttendency.ts
+++ src/hooks/useAttendency.ts
@@ -2,18 +2,20 @@ import { useEffect } from 'react';
 import { useAppDispatch } from './useAppDispatch';
 import { useAppSelector } from './useAppSelector';
 import useAuth from './useAuh';
-import { getAgents } from '@/redux/agents/agents.slice';
+import { getDaily } from '@/redux/attendency/attendency.slice';
 
-const useAgents = () => {
-	const { agents, status, message } = useAppSelector((state) => state.agents);
+const useAttendency = () => {
+	const { attendecies, status, message } = useAppSelector(
+		(state) => state.attendency
+	);
 	const dispatch = useAppDispatch();
 	const { isLogin } = useAuth();
 
 	useEffect(() => {
-		if (isLogin) dispatch(getAgents());
+		if (isLogin) dispatch(getDaily());
 		else console.log(message);
 	}, [dispatch, isLogin, message]);
-	return { agents, status, message };
+	return { attendecies, status, message };
 };
 
-export default useAgents;
+export default useAttendency;
