ea9ddf39d1e04c67d971d770242cc3e28019d8f2
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-08 08:38:33 +0200

    init general sidebar links

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 const MainLayout = ({
@@ -14,9 +15,7 @@ const MainLayout = ({
 			<div className='flex h-screen bg-[#f3f4f6]'>
 				<Sidebar />
 				<div className=' basis-full overflow-x-hidden h-screen  flex flex-col'>
-					<div className='bg-primary-800 md:w-full w-full flex justify-between items-center p-7 text-white'>
-						<div className='hidden md:flex gap-x-4 [&>.active]:border-b-4 border-white text-sm'></div>
-					</div>
+					<Navbar />
 					<div className='p-1 lg:p-10 overflow-y-auto max-w-screen'>
 						{children}
 					</div>
--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,10 +1,16 @@
 'use client';
 import type { CollapseProps } from 'antd';
-import { CaretRightOutlined } from '@ant-design/icons';
 import { Collapse } from 'antd';
 import { BiHome } from 'react-icons/bi';
 import { IoIosArrowDown } from 'react-icons/io';
+import {
+	PiCalendarLight,
+	PiCalendarPlusDuotone,
+	PiUsersFourThin,
+} from 'react-icons/pi';
 import { RxDashboard } from 'react-icons/rx';
+import { BsCalendarMonth } from 'react-icons/bs';
+import Link from 'next/link';
 
 const text = `
   A dog is a type of domesticated animal.
@@ -16,7 +22,7 @@ const items: CollapseProps['items'] = [
 	{
 		key: '1',
 		label: (
-			<div className='flex gap-2 items-center justify-between text-[#737a87] hover:bg-secondary-50 py-2 -my-1 rounded-md px-2'>
+			<div className='flex gap-2 items-center justify-between text-[#737a87]'>
 				<div className='flex gap-2 items-center text-[#737a87]'>
 					<BiHome className='text-xl' />
 					<span className='text-sm'>Home</span>
@@ -33,24 +39,7 @@ const items: CollapseProps['items'] = [
 	{
 		key: '2',
 		label: (
-			<div className='flex gap-2 items-center justify-between text-[#737a87] hover:bg-secondary-50 py-2 -my-1 rounded-md px-2'>
-				<div className='flex gap-2 items-center text-[#737a87]'>
-					<RxDashboard className='text-lg' />
-					<span className='text-sm'>Components</span>
-				</div>
-				<div>
-					<IoIosArrowDown className='text-lg' />
-				</div>
-			</div>
-		),
-		children: <p>{text}</p>,
-		showArrow: false,
-		// style: { marginRight: -10, marginLeft: -5 },
-	},
-	{
-		key: '3',
-		label: (
-			<div className='flex gap-2 items-center justify-between text-[#737a87] hover:bg-secondary-50 py-2 -my-1 rounded-md px-2'>
+			<div className='flex gap-2 items-center justify-between text-[#737a87]'>
 				<div className='flex gap-2 items-center text-[#737a87]'>
 					<RxDashboard className='text-lg' />
 					<span className='text-sm'>Components</span>
@@ -67,10 +56,6 @@ const items: CollapseProps['items'] = [
 ];
 
 const Sidebar = () => {
-	const icon = ({ isActive }: any) => {
-		// 'use server';
-		return <CaretRightOutlined rotate={isActive ? 90 : 0} />;
-	};
 	return (
 		<div className='h-screen z-10 w-80 bg-white overflow-y-auto relative py-2 border-r'>
 			<Collapse
@@ -79,17 +64,25 @@ const Sidebar = () => {
 				items={items}
 				bordered={false}
 				size='small'
-				// expandIconPosition='end'
-				// expandIcon={({
-				// 	isActive,
-				// 	style = {
-				// 		fontSize: '16px',
-				// 		// backgroundColor: 'red',
-				// 		color: '#737a87',
-				// 	},
-				// 	className = `${isActive && '-rotate-180 duration-300'} `,
-				// }) => <IoIosArrowDown style={style} className={className} />}
 			/>
+			<div className='text-[#737a87] text-[14.3px] mx-3 my-3'>
+				<Link href='soccase' className='flex gap-2 items-center'>
+					<PiUsersFourThin className='text-xl' />
+					Cas sociaux
+				</Link>
+			</div>
+			<div className='text-[#737a87] text-[14.3px] mx-3 my-4'>
+				<Link href='conge' className='flex gap-2 items-center'>
+					<PiCalendarPlusDuotone className='text-xl' />
+					Demande congé
+				</Link>
+			</div>
+			<div className='text-[#737a87] text-[14.3px] mx-3 my-4'>
+				<Link href='attend' className='flex gap-2 items-center'>
+					<PiCalendarLight className='text-xl' />
+					Mes présences
+				</Link>
+			</div>
 		</div>
 	);
 };
