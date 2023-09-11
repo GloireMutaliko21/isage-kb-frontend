d3ea0192342fb3c7672a1c6159298025876f437d
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-11 15:17:51 +0200

    feat: create useRemuneration hook

---
+++ src/hooks/useRemuneration.ts
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import useAuth from './useAuh';
+import {
+	getPayList,
+	getUnpaidAgents,
+} from '@/redux/remuneration/remuneration.slice';
+
+const useRemuneration = () => {
+	const paie = useAppSelector((state) => state.remuneration);
+	const dispatch = useAppDispatch();
+	const { isLogin } = useAuth();
+
+	useEffect(() => {
+		if (isLogin) {
+			dispatch(
+				getPayList({
+					month: new Date().getMonth(),
+					year: new Date().getFullYear(),
+				})
+			);
+			dispatch(
+				getUnpaidAgents({
+					month: new Date().getMonth(),
+					year: new Date().getFullYear(),
+				})
+			);
+		} else console.log(paie.message);
+	}, [dispatch, isLogin, paie.message]);
+	return { paie };
+};
+
+export default useRemuneration;
