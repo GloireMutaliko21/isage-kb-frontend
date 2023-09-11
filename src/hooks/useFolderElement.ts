c1d72765cd5a28614846ba9529dd6b265635d841
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-11 14:15:30 +0200

    feat: create useFolderElement hook

---
+++ src/hooks/useFolderElement.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import useAuth from './useAuh';
+import { getFolderElements } from '@/redux/folder-element/folder-element.slice';
+
+const useFolderElement = () => {
+	const { folderElements, message, selectedFoldEl, status } = useAppSelector(
+		(state) => state.folderElement
+	);
+	const dispatch = useAppDispatch();
+	const { isLogin } = useAuth();
+
+	useEffect(() => {
+		if (isLogin && !folderElements) dispatch(getFolderElements());
+		else console.log(message);
+	}, [dispatch, folderElements, isLogin, message]);
+	return { folderElements, selectedFoldEl, status, message };
+};
+
+export default useFolderElement;
