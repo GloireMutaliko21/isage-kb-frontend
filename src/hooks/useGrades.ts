be92f6a22ec147af095d1806836f403b6d06435d
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-11 14:18:17 +0200

    feat: create useGrade hook

---
+++ src/hooks/useGrades.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import useAuth from './useAuh';
+import { getGrades } from '@/redux/grade/grade.slice';
+
+const useGrades = () => {
+	const { grades, message, selectedGrade, status } = useAppSelector(
+		(state) => state.grade
+	);
+	const dispatch = useAppDispatch();
+	const { isLogin } = useAuth();
+
+	useEffect(() => {
+		if (isLogin && !grades) dispatch(getGrades());
+		else console.log(message);
+	}, [dispatch, grades, isLogin, message]);
+	return { grades, selectedGrade, status, message };
+};
+
+export default useGrades;
