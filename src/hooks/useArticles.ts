78cf20a6f9a887fd6c573e89d0d5fc122627cd77
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-11 14:02:36 +0200

    feat: create useArticles hook

---
+++ src/hooks/useArticles.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import useAuth from './useAuh';
+import { getArticles } from '@/redux/article/article.slice';
+
+const useArticles = () => {
+	const { articles, selectedArticle, message, status } = useAppSelector(
+		(state) => state.articles
+	);
+	const dispatch = useAppDispatch();
+	const { isLogin } = useAuth();
+
+	useEffect(() => {
+		if (isLogin && !articles) dispatch(getArticles());
+		else console.log(message);
+	}, [articles, dispatch, isLogin, message]);
+	return { articles, selectedArticle, status, message };
+};
+
+export default useArticles;
