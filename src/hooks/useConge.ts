01301f885c3a6da00140cbd90ccf1f9c6dcf955e
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-11 14:12:02 +0200

    feat: create useConge hook

---
+++ src/hooks/useConge.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import useAuth from './useAuh';
+import { getAgentsOnConge } from '@/redux/conge/conge.slice';
+
+const useConge = () => {
+	const { agentInConges, message, status } = useAppSelector(
+		(state) => state.conges
+	);
+	const dispatch = useAppDispatch();
+	const { isLogin } = useAuth();
+
+	useEffect(() => {
+		if (isLogin && !agentInConges) dispatch(getAgentsOnConge());
+		else console.log(message);
+	}, [agentInConges, dispatch, isLogin, message]);
+	return { agentInConges, status, message };
+};
+
+export default useConge;
