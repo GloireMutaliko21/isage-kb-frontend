1435d836375ff79b8739a26d4b89e18af5547637
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-11 15:20:28 +0200

    feat: create useRole hook

---
+++ src/hooks/useRole.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import useAuth from './useAuh';
+import { getRoles } from '@/redux/roles/role.slice';
+
+const useRole = () => {
+	const { message, roles, selectedRole, status } = useAppSelector(
+		(state) => state.role
+	);
+	const dispatch = useAppDispatch();
+	const { isLogin } = useAuth();
+
+	useEffect(() => {
+		if (isLogin && !roles) dispatch(getRoles());
+		else console.log(message);
+	}, [dispatch, isLogin, message, roles]);
+	return { roles, selectedRole, status, message };
+};
+
+export default useRole;
