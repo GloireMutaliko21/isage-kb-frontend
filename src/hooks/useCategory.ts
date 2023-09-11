b35e9fd7feeb360df85b5ddd772af1ff70ae6c9f
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-11 15:09:59 +0200

    feat: create useCategory hook

---
+++ src/hooks/useCategory.ts
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+import { useAppDispatch } from './useAppDispatch';
+import { useAppSelector } from './useAppSelector';
+import useAuth from './useAuh';
+import { getCategories } from '@/redux/property-category/category.slice';
+
+const useCategory = () => {
+	const { categories, message, selectedCategory, status } = useAppSelector(
+		(state) => state.category
+	);
+	const dispatch = useAppDispatch();
+	const { isLogin } = useAuth();
+
+	useEffect(() => {
+		if (isLogin && !categories) dispatch(getCategories());
+		else console.log(message);
+	}, [categories, dispatch, isLogin, message]);
+	return { categories, selectedCategory, status, message };
+};
+
+export default useCategory;
