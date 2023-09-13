2c240841ac84ac696ce86122e7fb5c445126e4a3
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-13 14:03:07 +0200

    feat: load grades cards

--- src/app/(app)/(admin)/config/g/page.tsx
+++ src/app/(app)/(admin)/config/g/page.tsx
@@ -1,15 +1,34 @@
+'use client';
+import CardGrade from '@/components/admin/CardGrade';
 import PageHeader from '@/components/global/PageHeader';
+import useGrades from '@/hooks/useGrades';
 import React from 'react';
 
-const grades = () => {
+const Grades = () => {
+	const { grades, status } = useGrades();
+
 	return (
 		<main className='flex flex-col h-full'>
 			<PageHeader title='Gestion des grades' />
 			<section className='p-5 flex-grow'>
-				<div className='bg-white p-5 rounded-lg h-full'>grades</div>
+				<div className='bg-white p-5 rounded-lg h-full'>
+					<div
+						className={`${
+							grades.length > 2 && 'justify-center'
+						} flex justify-start flex-wrap gap-2`}
+					>
+						{grades.map((grade) => (
+							<CardGrade
+								key={grade.id}
+								grade={grade}
+								loading={status.isLoading}
+							/>
+						))}
+					</div>
+				</div>
 			</section>
 		</main>
 	);
 };
 
-export default grades;
+export default Grades;
---
+++ src/components/admin/CardGrade.tsx
@@ -0,0 +1,32 @@
+import { Card, Skeleton } from 'antd';
+import { EditOutlined, EllipsisOutlined } from '@ant-design/icons';
+import { FcGraduationCap } from 'react-icons/fc';
+
+const { Meta } = Card;
+
+const CardGrade = ({ grade, loading }: { grade: Grade; loading: boolean }) => {
+	return (
+		<Card
+			style={{ width: 300, marginTop: 16 }}
+			actions={[<EditOutlined key='edit' />, <EllipsisOutlined key='more' />]}
+			className='shadow cursor-pointer duration-300 scale-95 hover:scale-100'
+		>
+			<Skeleton loading={loading} avatar active>
+				<Meta
+					avatar={<FcGraduationCap className='text-4xl' />}
+					title={grade.title}
+					description={
+						<p className='text-sm'>
+							Salaire de base:{' '}
+							<span className='font-bold text-secondary-800'>
+								{grade.baseSalary}
+							</span>
+						</p>
+					}
+				/>
+			</Skeleton>
+		</Card>
+	);
+};
+
+export default CardGrade;
