f81b4e9e60974f120eaad42631912e66cc52e47c
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-18 18:44:42 +0200

    feat: sections and services page load all

--- src/app/(app)/(admin)/config/s/page.tsx
+++ src/app/(app)/(admin)/config/s/page.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+'use client';
 
-const sectionService = () => {
-	return <div>sectionService</div>;
+import CardService from '@/components/admin/CardService';
+import PageHeader from '@/components/global/PageHeader';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import useServiceSection from '@/hooks/useServiceSection';
+import { openModal } from '@/redux/modalWindow/modalwindow.slice';
+
+const SectionService = () => {
+	const { services, status } = useServiceSection();
+	const dispatch = useAppDispatch();
+
+	const CreateServiceBtn = () => {
+		return (
+			<div className=''>
+				<button
+					onClick={() => dispatch(openModal({ modal_ID: 'NEW_SERVICE' }))}
+				>
+					Nouveau service
+				</button>
+			</div>
+		);
+	};
+	return (
+		<main className='flex flex-col h-full'>
+			<PageHeader title='Services' actionButton={<CreateServiceBtn />} />
+			<section className='p-5 flex-grow'>
+				<div className='bg-white p-5 rounded-lg h-full'>
+					<div
+						className={`${
+							services.length > 2 && 'justify-center'
+						} flex justify-start flex-wrap gap-2`}
+					>
+						{services.map((service) => (
+							<CardService
+								key={service.id}
+								service={service}
+								loading={status.isLoading}
+							/>
+						))}
+					</div>
+				</div>
+			</section>
+		</main>
+	);
 };
 
-export default sectionService;
+export default SectionService;
