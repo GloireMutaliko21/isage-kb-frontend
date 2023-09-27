f9987c68c89cb3e54edc5675e874105a67b9c85a
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-27 08:57:08 +0200

    refact: attency scanning flow

--- src/components/modals/CreateAttendency.tsx
+++ src/components/modals/CreateAttendency.tsx
@@ -1,19 +1,20 @@
 'use client';
+import { useState } from 'react';
 import { Button, Modal } from 'antd';
 import { QrReader } from 'react-qr-reader';
 
 import { useAppDispatch } from '@/hooks/useAppDispatch';
 import { createAttendency } from '@/redux/attendency/attendency.slice';
-import { closeModal } from '@/redux/modalWindow/modalwindow.slice';
 
 const CreateAttendency = ({ handlers }: { handlers: ModalsHandlers }) => {
 	const dispatch = useAppDispatch();
 	const onSubmit = (res: any, err: any) => {
 		if (!!res) {
 			dispatch(createAttendency({ agentId: res?.text, dateNow: new Date() }));
-			// dispatch(closeModal());
+			setScan(false);
 		}
 	};
+	const [scan, setScan] = useState<boolean>(false);
 	return (
 		<>
 			<Modal
@@ -23,7 +24,24 @@ const CreateAttendency = ({ handlers }: { handlers: ModalsHandlers }) => {
 				footer={null}
 				onCancel={() => handlers.close!(handlers.id!)}
 			>
-				<QrReader constraints={{ facingMode: 'user' }} onResult={onSubmit} />
+				<div className='flex justify-center items-center'>
+					{scan ? (
+						<div className='w-56 h-56'>
+							<QrReader
+								constraints={{ facingMode: 'user' }}
+								onResult={onSubmit}
+								className='w-full h-full'
+							/>
+						</div>
+					) : (
+						<button
+							className='bg-secondary-600 hover:bg-secondary-500 hover:shadow-lg duration-300 p-3 py-2 text-sm text-white rounded-md flex gap-2 justify-center items-center'
+							onClick={() => setScan(true)}
+						>
+							Ouvrir le scanner
+						</button>
+					)}
+				</div>
 				<div className='flex justify-end w-full gap-4'>
 					<Button
 						size='middle'
