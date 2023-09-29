a7a38728f8e71a4354483cf416b69d01412e6871
Gloire Mutaliko <mufunyig@gmail.com>
2023-09-29 16:40:32 +0200

    feat: add other fonts

--- src/docs/paySlip.ts
+++ src/docs/paySlip.ts
@@ -1,4 +1,4 @@
-import { font } from '@/assets/fonts';
+import { fontBold, fontMedium, fontNormal, fontSemiBold } from '@/assets/fonts';
 import jsPDF from 'jspdf';
 import autoTable from 'jspdf-autotable';
 import 'jspdf-autotable';
@@ -6,8 +6,14 @@ import 'jspdf-autotable';
 export async function generatePaySlip(id?: string) {
 	(function (jsPDFAPI: typeof jsPDF.API) {
 		const callAddFont = function (this: any) {
-			this.addFileToVFS('Cairo-Regular-normal.ttf', font);
+			this.addFileToVFS('Cairo-Regular-normal.ttf', fontNormal);
 			this.addFont('Cairo-Regular-normal.ttf', 'Cairo-Regular', 'normal');
+			this.addFileToVFS('Cairo-Medium-normal.ttf', fontMedium);
+			this.addFont('Cairo-Medium-normal.ttf', 'Cairo-Medium', 'normal');
+			this.addFileToVFS('Cairo-SemiBold-normal.ttf', fontSemiBold);
+			this.addFont('Cairo-SemiBold-normal.ttf', 'Cairo-SemiBold', 'normal');
+			this.addFileToVFS('Cairo-Bold-normal.ttf', fontBold);
+			this.addFont('Cairo-Bold-normal.ttf', 'Cairo-Bold', 'normal');
 		};
 		jsPDFAPI.events.push(['addFonts', callAddFont]);
 	})(jsPDF.API);
@@ -17,9 +23,11 @@ export async function generatePaySlip(id?: string) {
 	doc.text('Some Text with Google Fonts', 10, 10);
 	autoTable(doc, {
 		html: id,
-		styles: { font: 'Cairo-Regular', fontStyle: 'normal' },
+		styles: { font: 'Cairo-Regular' },
 		theme: 'grid',
-
+		columnStyles: {
+			0: { font: 'Cairo-Bold' },
+		},
 		useCss: true,
 	});
 	doc.save('liste.pdf');
