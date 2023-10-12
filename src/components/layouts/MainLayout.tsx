89f93360b5ab6d9a287bf63f9913551d845e0b49
Gloire Mutaliko <mufunyig@gmail.com>
2023-10-12 13:24:52 +0200

    feat: load disconnected page on app layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,15 +1,12 @@
+'use client';
+import useAuth from '@/hooks/useAuh';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
+import NotConnected from '../global/NotConnected';
 
-const MainLayout = ({
-	children,
-}: {
-	children:
-		| React.ReactNode
-		| React.ReactElement
-		| React.ReactNode[]
-		| React.ReactElement[];
-}) => {
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+	const { isLogin } = useAuth();
+
 	return (
 		<main>
 			<div className='flex h-screen bg-[#f3f4f6]'>
@@ -17,7 +14,7 @@ const MainLayout = ({
 				<div className=' basis-full overflow-x-hidden h-screen  flex flex-col'>
 					<Navbar />
 					<div className='overflow-y-auto max-w-screen flex-grow'>
-						{children}
+						{isLogin ? children : <NotConnected />}
 					</div>
 				</div>
 			</div>
