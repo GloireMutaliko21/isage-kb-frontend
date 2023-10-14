4c4faf0728b56fe5e3bfb6cccf147ee286853c8c
Gloire Mutaliko <mufunyig@gmail.com>
2023-10-14 10:04:05 +0200

    feat: load unities

--- src/app/(app)/(patrimoine)/inv/unit-cat/page.tsx
+++ src/app/(app)/(patrimoine)/inv/unit-cat/page.tsx
@@ -1,7 +1,70 @@
-import React from 'react';
+'use client';
+import { Tabs, type TabsProps } from 'antd';
 
-const page = () => {
-	return <div>page</div>;
+import PageHeader from '@/components/global/PageHeader';
+import { useAppDispatch } from '@/hooks/useAppDispatch';
+import { openModal } from '@/redux/modalWindow/modalwindow.slice';
+import Unities from '@/components/patrimoine/unity-category/Unities';
+import Categories from '@/components/patrimoine/unity-category/Categories';
+
+const UnityAndCategory = () => {
+	const dispatch = useAppDispatch();
+
+	const CreateCategory = () => {
+		return (
+			<div className=''>
+				<button
+					className='bg-secondary-600 p-2 text-white rounded-md text-sm'
+					onClick={() => dispatch(openModal({ modal_ID: 'NEW_CATEGORY' }))}
+				>
+					Nouvelle catégorie
+				</button>
+			</div>
+		);
+	};
+
+	const CreateUnity = () => {
+		return (
+			<div className=''>
+				<button
+					className='bg-secondary-600 p-2 text-white rounded-md text-sm'
+					onClick={() => dispatch(openModal({ modal_ID: 'NEW_UNITY' }))}
+				>
+					Nouvelle unité
+				</button>
+			</div>
+		);
+	};
+
+	const tabsItems: TabsProps['items'] = [
+		{
+			key: '1',
+			label: 'Liste catégories',
+			children: <Categories />,
+		},
+		{
+			key: '2',
+			label: 'Liste unités',
+			children: <Unities />,
+		},
+	];
+
+	return (
+		<main className='flex flex-col h-full'>
+			<div className='flex w-full justify-between items-center pr-5 border-b'>
+				<PageHeader title='Gestion des catégories et unités' />
+				<div className='flex gap-4'>
+					<CreateCategory />
+					<CreateUnity />
+				</div>
+			</div>
+			<section className='p-5 flex-grow'>
+				<div className='bg-white p-5 rounded-lg h-full'>
+					<Tabs defaultActiveKey='1' size='small' items={tabsItems} />
+				</div>
+			</section>
+		</main>
+	);
 };
 
-export default page;
+export default UnityAndCategory;
